62e00d96b86961ae6f587c9924d73f82ace94a37
Kirin Patel <kirinpatel@gmail.com>
2020-06-20 02:26:12 -0400

    Fixed sql syntax issues

--- src/controllers/deals.ts
+++ src/controllers/deals.ts
@@ -6,7 +6,7 @@ export const getCurrent = async (): Promise<Deal> => {
   const client = await getClient();
   return new Promise((resolve, reject) => {
     client.query(
-      `SELECT deals.created_at, deals.features, deals.id, deals.photo_urls as photos, deals.sold_out_at as soldOutAt, deals.specifications, deals.title, deals.url, deal_themes.accent_color as accentColor, deal_themes.background_color as backgroundColor, deal_themes.background_photo_url as backgroundImage, deal_themes.foreground as foreground, deal_stories.body as storyBody, deal_stories.title as storyTitle FROM deals
+      `SELECT deals.created_at, deals.features, deals.id, deals.photo_urls as photos, deals.sold_out_at as "soldOutAt", deals.specifications, deals.title, deals.url, deal_themes.accent_color as "accentColor", deal_themes.background_color as "backgroundColor", deal_themes.background_photo_url as "backgroundImage", deal_themes.foreground as foreground, deal_stories.body as "storyBody", deal_stories.title as "storyTitle" FROM deals
       JOIN deal_themes ON deals.id = deal_themes.deal_id
       JOIN deal_stories ON deals.id = deal_stories.deal_id
       WHERE created_at IS NOT NULL ORDER BY created_at DESC LIMIT 1;`,
@@ -59,12 +59,11 @@ export const insertOrUpdate = async (deal: Deal): Promise<unknown[]> => {
   const client = await getClient();
   return new Promise((resolve, reject) => {
     client.query(
-      `INSERT INTO deals(id, created_at, features, form_url, photo_urls, sold_out_at, specifications, title) VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+      `INSERT INTO deals(id, created_at, features, "url", photo_urls, sold_out_at, specifications, title) VALUES($1, $2, $3, $4, $5, $6, $7, $8)
       ON CONFLICT (id)
-      DO UPDATE SET features = $3, form_url = $4, photo_urls = $5, sold_out_at = $6, specifications = $7, title = $8;`,
+      DO UPDATE SET features = $3, "url" = $4, photo_urls = $5, sold_out_at = $6, specifications = $7, title = $8;`,
       [
         deal.id,
-        // TODO: Determine method of obtaining when a deal starts
         toTimestamp(deal.created_at),
         deal.features.toString(),
         deal.url,
